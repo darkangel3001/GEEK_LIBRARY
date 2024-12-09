f8741524531cda4e13638027f8a7c7c4ed12cd9d
darkangel3001 <dizum34@gmail.com>
2024-12-09 15:20:48 +0600

    Сделано 2 дзÐ

--- library_blog/models.py
+++ library_blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class BookModel(models.Model):
+    GENRE = (
+        ('Хоррор', 'Хоррор'),
+        ('Комедия', 'Комедия'),
+        ('Роман', 'Роман')
+    )
+    image = models.ImageField(upload_to='books/', verbose_name='Загрузите фото Книги')
+    title = models.CharField(max_length=100, verbose_name='Укажите название книги')
+    description = models.TextField(verbose_name='Укажите описание', blank=True)
+    price = models.FloatField(verbose_name='Задайте цену книги', default=25)
+    created_at = models.DateField(auto_now_add=True)
+    genre = models.CharField(max_length=100, choices=GENRE, default='Роман')
+    author_gmail = models.EmailField(verbose_name='Укажите эл.почту автора')
+    author = models.CharField(max_length=100, verbose_name='Укажите автора', default='Уильям Шекспир')
+    audio_book = models.URLField(verbose_name='Укажите ссылку аудио книги')
+
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return self.title
--- library_blog/views.py
+++ library_blog/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 import datetime
+from . import models
+
+def book_detail_view(request, id):
+    if request.method == 'GET':
+        book_id = get_object_or_404(models.BookModel, id=id)
+        context = {
+            'book_id': book_id,
+            }
+        return render(request, template_name='book_detail.html', context=context)
+
+
+def book_list_view(request):
+    if request.method == 'GET':
+        book_list = models.BookModel.objects.all().order_by('-id')
+        context = {
+            'book_list': book_list
+        }
+        return render(request,template_name='book.html', context=context)
+
+
+
 
 def about_me(request):
     if request.method == 'GET':
